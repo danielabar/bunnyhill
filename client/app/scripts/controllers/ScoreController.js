eefe12352c8a9f23ab93081093a47e897b1e20db
Daniela Baron <baron.daniela@gmail.com>
2013-11-30 16:50:03 -0500

    enhance score view to display deck name, level, and played date

--- client/app/scripts/controllers/ScoreController.js
+++ client/app/scripts/controllers/ScoreController.js
@@ -9,16 +9,4 @@ angular.module('meanRecipieApp')
 			$scope.scoreBoard = res;
 		});
 
-		// var isEmpty = function(obj) {
-  //   	return Object.keys(obj).length === 0;
-		// }
-		
-		// $scope.scoreBoard = GameService.getScoreBoard();
-		// if(isEmpty($scope.scoreBoard)) {
-		// 	$scope.scoreBoard = angular.fromJson(localStorageService.get($scope.name));
-		// 	if(isEmpty($scope.scoreBoard)) {
-		// 		$location.path('/');
-		// 	}
-		// }
-
 	});

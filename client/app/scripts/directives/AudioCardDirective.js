50aa6e93672b226a81f8ddc0ceccf8690fe4ce21
Daniela Baron <baron.daniela@gmail.com>
2013-12-01 15:28:39 -0500

    wip on intermediate audio game play

--- client/app/scripts/directives/AudioCardDirective.js
+++ client/app/scripts/directives/AudioCardDirective.js
@@ -6,23 +6,52 @@ angular.module('meanRecipieApp')
       restrict: 'E',
       scope: {
       	message: "=message",
-      	carddata: "="
+      	carddata: "=",
+      	behaviour: "="
       },
       template:
       	'<div class="card"> ' +
-      		'<div class="span2">Display Test</div>' +
+      		'<div class="span2">{{message}}</div>' +
       		'<img src="img/audio.gif" class="img-circle" height="42" width="42">' +
+      		'<div id="feedbackMarkerSuccess" class="circle circle-success">&#x2713;</div>' +
+      		'<div id="feedbackMarkerError" class="circle circle-error">&#x2718;</div>' +
       	'</div>',
       replace: true,
 
       link: function postLink(scope, element, attrs) {
 
+      	var hideMarker = function() {
+      		$('#feedbackMarkerSuccess').hide();
+      		$('#feedbackMarkerError').hide();
+      	};
+
+      	var displayMarker = function(result) {
+      		if (result) {
+      			$('#feedbackMarkerSuccess').show();
+      		} else {
+      			$('#feedbackMarkerError').show();
+      		}
+      	};
+
       	var audioElement = $document[0].createElement('audio');
+
+      	hideMarker();
 
       	scope.$watch('carddata', function(data) {
       		if(data && data.audio) {
+      			scope.carddata = data;
+      			scope.message = 'Enter the word exactly as you just heard';
       			audioElement.src = data.audio;
       			audioElement.play();
+      		}
+      	});
+
+      	scope.$watch('behaviour', function(data) {
+      		if (data && data.flipCard) {
+      			scope.message = 'Answer is:' + scope.carddata.translated;
+      			displayMarker(data.isAnswerCorrect);
+      		} else {
+      			hideMarker();
       		}
       	});
 
